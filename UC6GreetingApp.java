80b1ac9a6e7d8cf8d32121bf4c08b5d878790e24
abishek kumaravel <abishekkumaravel@abisheks-MacBook-Air.local>
2026-04-10 08:59:37 +0530

    UC6: Greeting app using substring method

---
+++ UC6GreetingApp.java
@@ -0,0 +1,19 @@
+public class UC6GreetingApp {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String result = "Hello ";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println(result);
+
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
